79e6a0742cf3be7852aa8815eb4b033201302aea
matthias haefeli <haefeli.matthias@gmail.com>
2020-06-24 13:19:42 -0600

    final section3

--- src/screens/ListScreeen.js
+++ src/screens/ListScreeen.js
@@ -18,7 +18,11 @@ const ListScreen = () => {
       keyExtractor={(friend) => friend.name}
       data = {friends}
       renderItem={({ item }) => {
-      return <Text style={styles.textStyle}>{item.name} - Age {item.age}</Text>
+        return (
+          <Text style={styles.textStyle}>
+            {item.name} - Age {item.age}
+          </Text>
+        )
       }}
     />
   )
