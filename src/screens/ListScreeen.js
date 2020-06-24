e29124fc4d6256f906587415e5df36b0050d5210
matthias haefeli <haefeli.matthias@gmail.com>
2020-06-24 13:13:18 -0600

    add list of objects

--- src/screens/ListScreeen.js
+++ src/screens/ListScreeen.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = () => {
+  const friends = [
+    { name: 'Friend 1' },
+    { name: 'Friend 2' },
+    { name: 'Friend 3' },
+    { name: 'Friend 4' },
+    { name: 'Friend 5' },
+    { name: 'Friend 6' },
+    { name: 'Friend 7' },
+    { name: 'Friend 8' },
+  ]
+
   return (
-    <Text Text>List Screen </Text>
+    <FlatList
+      keyExtractor={(friend) => friend.name}
+      data = {friends}
+      renderItem={({ item }) => {
+      return <Text style={styles.textStyle}>{item.name}</Text>
+      }}
+    />
   )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 50
+  }
+});
 
 export default ListScreen;
