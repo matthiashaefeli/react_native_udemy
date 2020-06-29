04c24211e3d1bc19832cbc3817fccbf8b38ef603
matthias haefeli <haefeli.matthias@gmail.com>
2020-06-29 07:55:45 -0600

    start section 6

--- App.js
+++ App.js
@@ -3,14 +3,16 @@ import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from "./src/screens/HomeScreen";
 import ComponentsScreen from "./src/screens/ComponentsScreen";
 import ListScreen from "./src/screens/ListScreeen";
-import ImageScreen from "./src/screens/ImageScreen"
+import ImageScreen from "./src/screens/ImageScreen";
+import CounterScreen from "./src/screens/CounterScreen";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
     Components: ComponentsScreen,
     List: ListScreen,
-    Image: ImageScreen
+    Image: ImageScreen,
+    Counter: CounterScreen
   },
   {
     initialRouteName: "Home",
