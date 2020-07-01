23f8f256db87abbdaeb4065b6d6af29e562aa915
matthias haefeli <haefeli.matthias@gmail.com>
2020-07-01 16:31:16 -0600

    add square screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -25,6 +25,10 @@ const HomeScreen = ({ navigation }) => {
           title="Go to Color"
           onPress={() => navigation.navigate('Color') }
         />
+        <Button
+          title="Go to Square"
+          onPress={() => navigation.navigate('Square') }
+        />
       </View>
     )
 };
