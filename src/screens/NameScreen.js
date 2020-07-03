076e609b412cdd904ef8cf6b6528f72d3d842bfa
matthias haefeli <haefeli.matthias@gmail.com>
2020-07-03 13:11:03 -0600

    add textinput and style

--- src/screens/NameScreen.js
+++ src/screens/NameScreen.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
 
 const NameScreen = () => {
   return (
     <View>
-      <Text>Name Screen</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize='none'
+        autoCorrect={false}
+      />
     </View>
   )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: 'black',
+    borderWidth: 1
+  }
+});
 
 export default NameScreen;
