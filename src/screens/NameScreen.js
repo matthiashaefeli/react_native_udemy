bb4f0fa9f89b76eaa1a065c3441962202ce5a743
matthias haefeli <haefeli.matthias@gmail.com>
2020-07-03 13:40:16 -0600

    change to password with validation

--- src/screens/NameScreen.js
+++ src/screens/NameScreen.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View, StyleSheet, TextInput } from 'react-native';
 
 const NameScreen = () => {
+  const [password, setPassword] = useState('');
+
   return (
     <View>
+      <Text>Enter Password</Text>
       <TextInput
         style={styles.input}
         autoCapitalize='none'
         autoCorrect={false}
+        value={password}
+        onChangeText={ (text) => setPassword(text)}
       />
+      { password.length < 5 ? <Text>Password must be longer than 5 characters!</Text> : null }
     </View>
   )
 };
